b1f7da426206f53b9268d3f8b56b9d6660a63c8a
Anurag Guda <anuragmsu@gmail.com>
2021-03-05 21:57:23 +0000

    kubernetes new dashboards

---
+++ dashboard/README.md
@@ -0,0 +1,22 @@
+# Kubernetes Dashboards 
+
+Here we're deploying two different kind of kubernetes dashboards
+1. [Kubernetes Dashboard](https://github.com/kubernetes/dashboard)
+2. [K8sdash](https://github.com/indeedeng/k8dash)
+
+## Install
+Run the below command to install kubernetes dashboards
+```
+bash dashboard.sh create
+```
+
+## Access
+
+- Access the dashboard URL's from output of the above script with provided token
+
+## Clean Up
+
+Run the below command to clean up the dashboards
+```
+bash dashboard.sh delete
+```
--- dashboard/dashboard.sh
+++ dashboard/dashboard.sh
@@ -1,6 +1,8 @@
 if [[ $1 == 'create' ]] ; then
 	kubectl create -f k8dashboard.yaml
 	kubectl create -f k8sdashboard.yaml
+	kubectl create -f kubernetes-k8dash.yaml
+        kubectl create -f metric-server.yaml
 	echo
 	export hostIP=$(kubectl get nodes --namespace kubernetes-dashboard -o jsonpath="{.items[0].status.addresses[0].address}{'\n'}")
 	export nodePort=$(kubectl get svc kubernetes-dashboard -n kubernetes-dashboard -o jsonpath="{.spec.ports[?(@.port=="443")].nodePort}{'\n'}")
@@ -11,9 +13,22 @@ if [[ $1 == 'create' ]] ; then
 	echo
 	echo -e "Dashboard Token: 	$token"
 	echo
+	kubectl create serviceaccount k8dash-sa
+	kubectl create clusterrolebinding k8dash-sa --clusterrole=cluster-admin --serviceaccount=default:k8dash-sa
+	export k8sdashtoken=$(kubectl describe secret $(kubectl get secrets | grep k8dash-sa | awk '{print $1}') | grep -iw 'token:'| awk '{print $2}')
+	echo
+	export k8sdashnodePort=$(kubectl get svc k8dash  -n kube-system -o jsonpath="{.spec.ports[?(@.port=="80")].nodePort}{'\n'}")
+	echo -e "Access Dashboard Using http://$hostIP:$k8sdashnodePort"
+	echo
+	echo -e "Dashboard Token:       $k8sdashtoken"
+	echo
 elif [[ $1 == 'delete' ]]; then
 	kubectl delete -f k8dashboard.yaml
 	kubectl delete -f k8sdashboard.yaml
+	kubectl delete -f kubernetes-k8dash.yaml
+	kubectl delete -f metric-server.yaml
+	kubectl delete clusterrolebinding k8dash-sa
+	kubectl delete serviceaccount k8dash-sa
 else
 	echo -e "USAGE:
 
